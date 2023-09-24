57a0ab7a368a2706c4a16f25b86884c0c33cb626
zzfn <44751363+zzfn@users.noreply.github.com>
2023-09-25 00:20:30 +0800

    feat: httpClient

--- kafka/article_update_consumer.go
+++ kafka/article_update_consumer.go
@@ -8,35 +8,50 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
-func ArticleUpdateHandler(msg kafka.Message) {
-	fmt.Printf("Processing article update: %s = %s\n", string(msg.Key), string(msg.Value))
-	client := &http.Client{}
-	secret := os.Getenv("NEXT_SECRET")
+func SendRequest(client *http.Client, apiPath string, requestData []byte) ([]byte, error) {
 	nextPublicBaseUrl := os.Getenv("NEXT_PUBLIC_BASE_URL")
-	data := []byte(`{"tag": ["article"],"secret": "` + secret + `"}`)
-	req, err := http.NewRequest("POST", nextPublicBaseUrl+"/api/revalidateTag", bytes.NewBuffer(data))
+	req, err := http.NewRequest("POST", nextPublicBaseUrl+apiPath, bytes.NewBuffer(requestData))
 	if err != nil {
-		fmt.Printf("Error creating request: %v\n", err)
-		return
+		return nil, fmt.Errorf("Error creating request: %v", err)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Error sending request: %v\n", err)
-		return
+		return nil, fmt.Errorf("Error sending request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Error reading response body: %v\n", err)
+		return nil, fmt.Errorf("Error reading response body: %v", err)
+	}
+
+	return body, nil
+}
+
+func ArticleUpdateHandler(msg kafka.Message) {
+	fmt.Printf("Processing article update: %s = %s\n", string(msg.Key), string(msg.Value))
+	httpClient := &http.Client{Timeout: 10 * time.Second} // defining the http client here
+	secret := os.Getenv("NEXT_SECRET")
+	data := []byte(fmt.Sprintf(`{"tag": ["article"],"secret": "%s"}`, secret))
+	body, err := SendRequest(httpClient, "/api/revalidateTag", data)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	log.Info("Request Body", string(body))
+	log.Info("Response Body from first API:", string(body))
+	data = []byte(fmt.Sprintf(`{"path": ["/post/%s"],"secret": "%s"}`, msg.Value, secret))
+	body, err = SendRequest(httpClient, "/api/revalidatePath", data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	log.Info("Response Body from first API:", string(body))
 }
 
 func NewArticleUpdateConsumer() *Consumer {
