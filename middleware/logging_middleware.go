065d045cfcf938ee4d50e50815bb1ad8b705ab15
cc <44751363+zzfn@users.noreply.github.com>
2024-02-07 21:15:17 +0800

    feat: kafka

--- middleware/logging_middleware.go
+++ middleware/logging_middleware.go
@@ -1,10 +1,9 @@
 package middleware
 
 import (
-	"log"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"time"
 )
 
 type RequestIPHeaders struct {
@@ -27,10 +26,8 @@ func LoggingMiddleware(c *fiber.Ctx) error {
 	if err := c.ReqHeaderParser(p); err != nil {
 		return err
 	}
-	log.Printf("Client IP: %s | Forwarded IPs: %v | X-Forwarded-For: %s | X-Real-Ip: %s ｜ Cf-Connecting-Ip: %s\n",
-		c.IP(), c.IPs(), p.XForwardedFor, p.XRealIp, p.CfConnectingIp)
 	// 打印请求方法、路径和耗时
-	log.Printf("[%s] %s - %v - IP: %s\n", c.Method(), c.Path(), duration, p.CfConnectingIp)
+	log.Info("[%s] %s - %v - IP: %s\n", c.Method(), c.Path(), duration, p.CfConnectingIp)
 
 	return err
 }
