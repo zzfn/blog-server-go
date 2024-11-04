118d600a7fb4e40ca597b723a8d8905772d2ebca
cc <44751363+zzfn@users.noreply.github.com>
2024-11-04 15:16:06 +0800

    refactor(models): 精简 Task 结构体字段

--- models/task.go
+++ models/task.go
@@ -2,9 +2,8 @@ package models
 
 type Task struct {
 	BaseModel
-	Type     string  `json:"type"`
-	Title    string  `json:"title"`
-	Amount   float64 `json:"amount"`
-	Cycle    string  `json:"cycle"`
-	Nickname string  `json:"nickname"`
+	Type   string  `json:"type"`
+	Title  string  `json:"title"`
+	Amount float64 `json:"amount"`
+	Cycle  string  `json:"cycle"`
 }
